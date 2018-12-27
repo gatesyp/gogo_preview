317a6dba6f1355cddc78896cf9b2a922cc5820c7
gatesyp <steven@deephire.com>
2018-12-27 03:31:05 -0500

    added base linkedin preview chrome extension

---
+++ content.js
@@ -0,0 +1,19 @@
+window.onclick = function() {
+    links = document.getElementsByTagName("a");
+    javascriptString = '<script>displayPopup() {  };</script>'
+    for(var i = 0; i < links.length; i++) { 
+        if ( links[i].href.includes("linkedin.com") ) {
+            stevenbox = document.getElementById("STEVENBOX")
+            links[i].setAttribute("id", 'STEVENLINK');
+            links[i].setAttribute('onmouseover', 'document.getElementById("STEVENBOX").style.display = "block";');
+            links[i].setAttribute('onmouseout', 'document.getElementById("STEVENBOX").style.display = "none";');
+            if(stevenbox){
+                stevenbox.innerHTML = '<iframe src="' + links[i].href + '" width = "1000px" height = "500px"></iframe>'
+            } else {
+                htmlString = '<div id="STEVENBOX" style="z-index: 100;position: absolute;display: none;width: 100%;}"><iframe src="' + links[i].href + '" width = "500px" height = "500px"></iframe></div>'
+                links[i].insertAdjacentHTML('beforeend', htmlString)
+            }
+            links[i].insertAdjacentHTML('beforeend', javascriptString)
+        }
+    }
+}
