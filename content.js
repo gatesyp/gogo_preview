19dfeb565f03c13dc79b52b5895fb47faeb8abe3
agates4 <agate97@gmail.com>
2019-01-01 17:30:41 -0500

    changed width to 1000

--- content.js
+++ content.js
@@ -8,9 +8,9 @@ window.onclick = function() {
             links[i].setAttribute('onmouseover', 'document.getElementById("STEVENBOX").style.display = "block";');
             links[i].setAttribute('onmouseout', 'document.getElementById("STEVENBOX").style.display = "none";');
             if(stevenbox){
-                stevenbox.innerHTML = '<iframe src="' + links[i].href + '" width = "1000px" height = "500px"></iframe>'
+                stevenbox.innerHTML = '<iframe src="' + links[i].href + '" width = "1000px" height = "1000px"></iframe>'
             } else {
-                htmlString = '<div id="STEVENBOX" style="z-index: 100;position: absolute;display: none;width: 100%;}"><iframe src="' + links[i].href + '" width = "500px" height = "500px"></iframe></div>'
+                htmlString = '<div id="STEVENBOX" style="z-index: 100;position: absolute;display: none;width: 100%;}"><iframe src="' + links[i].href + '" width = "1000px" height = "500px"></iframe></div>'
                 links[i].insertAdjacentHTML('beforeend', htmlString)
             }
             links[i].insertAdjacentHTML('beforeend', javascriptString)
